4255af8d297b90ab2c8c866f6ac1c2e9a0a8fb7f
jpmmatias <jpmmatias2018@outlook.com>
2023-06-22 22:08:42 -0300

    feat: :lipstick: create jumbtron component
    created jumbtron section component

---
+++ src/components/Jumbotron.tsx
@@ -0,0 +1,29 @@
+import IPhone from '../assets/images/iphone-14.jpg';
+import HoldingIPhone from '../assets/images/iphone-hand.png';
+const Jumbotron = () => {
+	return (
+		<section className='jumbotron-section wrapper'>
+			<h2 className='title'>New</h2>
+			<img src={IPhone} className='logo' alt='IPhone 14 Pro' />
+			<p className='text'>Big and bigger.</p>
+			<span className='description'>
+				from $699 before trade-in or $29.12/mo. for 24 mo. <sup>1</sup>
+			</span>
+			<ul className='links'>
+				<li>
+					<button className='button'>Buy</button>
+				</li>
+				<li>
+					<a className='link'>Learn more</a>
+				</li>
+			</ul>
+			<img
+				src={HoldingIPhone}
+				className='holding-iphone'
+				alt='Holding IPhone 14 Pro'
+			/>
+		</section>
+	);
+};
+
+export default Jumbotron;
