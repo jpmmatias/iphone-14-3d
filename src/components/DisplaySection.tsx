3e10fca821f6b3428b44647ab3a4dcbff2dc0d76
jpmmatias <jpmmatias2018@outlook.com>
2023-06-22 22:31:21 -0300

    feat: :lipstick: Add Display Section
    Added display section UI

---
+++ src/components/DisplaySection.tsx
@@ -0,0 +1,16 @@
+const DisplaySection = () => {
+	return (
+		<section className='display-section wrapper'>
+			<h2 className='title'>New</h2>
+			<p className='text'>Brillant.</p>
+			<span className='description'>
+				A display that's up to 200% brighter outdoors when you need it most.{' '}
+				<sup>1</sup>
+			</span>
+			<button className='button'>Try me!</button>
+			<button className='back-button'>TOP</button>
+		</section>
+	);
+};
+
+export default DisplaySection;
